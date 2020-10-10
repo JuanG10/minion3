ed0c41ccbb61e64ec2efed77e46ca16314de597d
Alejandro214 <alejandromamanijatabe@gmail.com>
2020-10-10 14:27:52 -0300

    cambio implementacion cadena

--- Cadena.gd
+++ Cadena.gd
@@ -1,2 +1,12 @@
 extends Sprite
 
+var velocidad = 100
+
+func _physics_process(delta):
+	print(delta)
+	set_scale(Vector2(get_scale().x,velocidad * delta))
+	#transform.scaled(Vector2(get_transform().get_scale().x,500))
+	set_offset(Vector2(get_offset().x,velocidad * delta))
+	velocidad += 1
+	print(get_offset())
+
--- CadenaYGancho.gd
+++ CadenaYGancho.gd
@@ -1,24 +1,39 @@
 extends Node
 export (PackedScene) var cadena_escena
 
-var n = 50
+var n = 10
+var cant_cadenas = 0
+var subiendo = false
+
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		$Timer.start(0.1)
+	elif Input.is_action_just_pressed("ui_down"):
+		$Timer.start(0.1)
+		subiendo = true
 
 		
 
 func bajar_cadena():
-			$Timer.start(0.1)
+			#$Timer.start(0.1)
 			var cadena = cadena_escena.instance()
 			cadena.position = get_child(0).position
-			get_child(0).position.y += 1
-			get_child(1).position.y += 1
+			get_child(0).position.y += 5
+			get_child(1).position.y += 5
 			add_child(cadena)
+			cant_cadenas += 1
+			print(get_child(cant_cadenas))
 
 			
+func subir_cadena():
+	if get_child_count()  > 3:
+		print(get_child_count())
+		print(get_child(cant_cadenas))
+		remove_child(get_child(cant_cadenas))
+		cant_cadenas -= 1
 	
+		
 
 	
 
@@ -26,5 +41,7 @@ func _on_Timer_timeout():
 	if(n > 0):
 		bajar_cadena()
 		n -= 1
+	elif subiendo:
+		subir_cadena()	
 
    
