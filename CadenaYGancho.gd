aa03d6b04fc224ff094859f012594d774e50a7ef
Alejandro214 <alejandromamanijatabe@gmail.com>
2020-10-09 22:41:08 -0300

    cambios en la cadena

--- CadenaYGancho.gd
+++ CadenaYGancho.gd
@@ -1,21 +1,30 @@
 extends Node
 export (PackedScene) var cadena_escena
 
-var n = 10
+var n = 50
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
-		while(n != 0):
-			print(n)
-			bajar_cadena()
-			n -= 1
+		$Timer.start(0.1)
 
+		
 
 func bajar_cadena():
+			$Timer.start(0.1)
 			var cadena = cadena_escena.instance()
 			cadena.position = get_child(0).position
 			get_child(0).position.y += 1
 			get_child(1).position.y += 1
 			add_child(cadena)
+
 			
 	
+
+	
+
+func _on_Timer_timeout():
+	if(n > 0):
+		bajar_cadena()
+		n -= 1
+
+   
--- Palanca.gd
+++ Palanca.gd
@@ -5,7 +5,5 @@ var bajo_cadena = false
 func _process(delta):
 	if Input.is_action_just_pressed("ui_down"):
 		frame = 0
-		if(!bajo_cadena):
-			while(5):
-				get_parent().get_node("CadenaYGancho").bajar_cadena()
-				get_parent().get_node("CadenaYGancho").bajar_cadena()
+		get_parent().get_node("CadenaYGancho").bajar_cadena()
+			
