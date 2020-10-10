6572a9c5e896feab4ca1b318cac26b1036a25694
Alejandro214 <alejandromamanijatabe@gmail.com>
2020-10-10 16:22:22 -0300

    repeticion de textura

--- Cadena.gd
+++ Cadena.gd
@@ -1,12 +1,11 @@
 extends Sprite
 
-var velocidad = 100
+var velocidad = 1000
 
 func _physics_process(delta):
 	print(delta)
-	set_scale(Vector2(get_scale().x,velocidad * delta))
-	#transform.scaled(Vector2(get_transform().get_scale().x,500))
-	set_offset(Vector2(get_offset().x,velocidad * delta))
-	velocidad += 1
+	region_rect = Rect2(0, 0, 2,  velocidad  * delta)
+	set_offset(Vector2(get_offset().x,(velocidad  * delta)/ 2))
+	velocidad += 10
 	print(get_offset())
 
