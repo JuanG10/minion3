c06276942def50a2e69934fc7ffb71f09d0d9c9d
Alejandro214 <alejandromamanijatabe@gmail.com>
2020-10-11 19:32:17 -0300

    refactor

--- Cadena.gd
+++ Cadena.gd
@@ -19,12 +19,15 @@ func _physics_process(delta):
 		velocidad += 10
 		bajar_cadena -= 1
 	if subir_cadena > 0 && esta_subiendo:
-		print('hola')
 		subirBajar()
 		velocidad -= 10
 		bajar_cadena += 1
 		subir_cadena -= 1
 
+
+func activar():
+	region_rect = Rect2(0,0,region_rect.size.x, aceleracion)
+	set_offset(Vector2(get_offset().x,aceleracion / 2))
 func subirBajar():
 	region_rect = Rect2(0,0,region_rect.size.x, aceleracion)
 	set_offset(Vector2(get_offset().x,aceleracion / 2))
