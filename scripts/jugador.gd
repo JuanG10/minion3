147f9973dae748e7f74e83ea43f1173b68fb5a50
JuanG10 <juangaray463@gmail.com>
2020-10-12 18:18:42 -0300

    algunosComentariosYMiniRefactorCodigo

--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -14,28 +14,31 @@ func _physics_process(delta):
 	get_input()
 	move_and_slide(velocity)
 
-func get_input():
+func get_input(): # Obtiene el input para moverse.
 	velocity = Vector2()
 	if !is_on_floor() && !can_climb:
 		velocity.y += 1
 	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-		player_spr.play("movimiento_horizontal")
-		player_spr.flip_h = false
+		_move_left()
 	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-		player_spr.play("movimiento_horizontal")
-		player_spr.flip_h = true
+		_move_right()
 	if _input_release():
 		player_spr.play("idle")
 	velocity = velocity.normalized() * speed
 
-func _input_release():
-	return Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_left")
+func _input_release(): # Chequea si se sueltan teclas direccionales.
+	return Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_up")
 
-func _input_up():
-	return Input.is_action_pressed("ui_up") && can_climb
+func _move_left():
+	velocity.x += 1
+	player_spr.play("movimiento_horizontal")
+	player_spr.flip_h = false
+	
+func _move_right():
+	velocity.x -= 1
+	player_spr.play("movimiento_horizontal")
+	player_spr.flip_h = true
 
 func can_climb_up(cadena_pos):
-	if _input_up():
+	if Input.is_action_pressed("ui_up") && can_climb:
 		position.y -= 1
