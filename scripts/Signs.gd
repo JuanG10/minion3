c7c97860ae3576e82bffb780afde3abf8b7a1c4e
JuanG10 <juangaray463@gmail.com>
2020-10-16 18:12:24 -0300

    comentariosYPequeñasModificacionesALosSignos

--- scripts/Signs.gd
+++ scripts/Signs.gd
@@ -4,30 +4,32 @@ onready var exclamation = preload("res://sprites/exclamation.png")
 onready var question = preload("res://sprites/question.png")
 
 const START_HEIGHT:Vector2 = Vector2(0,12)
+const TIME_Q_SIGN:float = 0.75 # Tiempos para pasar al mensaje _create_sign,
+const TIME_E_SIGN:float = 0.4 # Son parámetros para que sea reutilizable.
 var timer_name:String
 var spr_name:String
 
 func start_exclamation_sign(pos:Vector2, new_timer_name:String, new_spr_name:String)->void:
-	_create_sign(pos, exclamation, new_timer_name, new_spr_name)
+	_create_sign(pos, exclamation, new_timer_name, new_spr_name, TIME_E_SIGN)
 
 func start_question_sign(pos:Vector2, new_timer_name:String, new_spr_name:String)->void:
-	_create_sign(pos, question, new_timer_name, new_spr_name)
+	_create_sign(pos, question, new_timer_name, new_spr_name, TIME_Q_SIGN)
 
-func _create_sign(pos:Vector2, image, new_timer_name:String, new_spr_name:String)->void:
+func _create_sign(pos:Vector2, image, new_timer_name:String, new_spr_name:String, time:float)->void:
 	timer_name = new_timer_name
 	spr_name = new_spr_name
 	var root:Node = get_tree().get_root()
-	var timer:Timer = _create_timer()
+	var timer:Timer = _create_timer(time)
 	var new_sign:Sprite = _create_sprite(image)
 	root.add_child(new_sign)
 	root.add_child(timer)
 	timer.start()
 	new_sign.position = pos - START_HEIGHT
 
-func _create_timer()->Timer:
+func _create_timer(time)->Timer:
 	var new_timer:Timer = Timer.new()
 	new_timer.set_one_shot(true)
-	new_timer.set_wait_time(0.35)
+	new_timer.set_wait_time(time)
 	new_timer.connect("timeout",self,"_on_timer_timeout")
 	new_timer.set_name(timer_name)
 	return new_timer
