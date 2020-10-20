823d903eeba4bb9da0b5e1315bb69364bed84d23
JuanG10 <juangaray463@gmail.com>
2020-10-19 23:35:57 -0300

    seRompíeronLosPisos

--- scripts/Suelo_RayCast.gd
+++ scripts/Suelo_RayCast.gd
@@ -2,5 +2,7 @@ extends RayCast2D
 
 func _physics_process(delta):
 	var collider = get_collider()
-	if collider != null && collider.get_class() == "KinematicBody2D":
+	if collider != null:
+		print(collider, " ", collider.get_name())
+		if collider.get_class() == "KinematicBody2D":
 			get_collider().velocity.y = 0
