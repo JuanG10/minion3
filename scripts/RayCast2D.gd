8eec42aedc0b4eb98e2624ad267d11ac6d253731
Alejandro214 <alejandromamanijatabe@gmail.com>
2020-10-20 11:27:32 -0300

    errores solucionados

--- scripts/RayCast2D.gd
+++ scripts/RayCast2D.gd
@@ -3,5 +3,7 @@ extends RayCast2D
 onready var cadena = get_parent().get_parent().get_parent().get_node("Cadena")
 
 func _physics_process(delta):
-	if get_collider() != null && get_collider().get_class() == "TileMap":
+	print(get_collider())
+	if get_collider() != null && get_collider().get_class() == "KinematicBody2D":
+		print ("entra")
 		cadena.puede_subir = true
