f6d6e238b6e7aa91d8d37945d4d17093b8d9a48b
Alejandro214 <alejandromamanijatabe@gmail.com>
2020-10-20 11:34:48 -0300

    cambio en script

--- scripts/RayCast2D.gd
+++ scripts/RayCast2D.gd
@@ -3,7 +3,5 @@ extends RayCast2D
 onready var cadena = get_parent().get_parent().get_parent().get_node("Cadena")
 
 func _physics_process(delta):
-	print(get_collider())
-	if get_collider() != null && get_collider().get_class() == "KinematicBody2D":
-		print ("entra")
+	if get_collider() != null && get_collider().get_class() == "TileMap":
 		cadena.puede_subir = true
